f977509fdbf6b640d7dd79e624b5d9832fbde772
hans torres <hans.torres@98labs.com>
2024-04-15 20:29:29 +0800

    add func 2

--- main.go
+++ main.go
@@ -1,5 +1,44 @@
 package passlock
 
+import (
+	"crypto/rand"
+	"math/big"
+)
+
+type PasswordType int
+
+const (
+	Random PasswordType = iota
+	AlphaNumeric
+	Pin
+)
+
 func GeneratePassword() string {
-	return "abc123"
+	return generate(12, false, false, false, 0)
+}
+
+func generate(passwordLength int, includeNumbersFlag bool, includeSymbolsFlag bool, includeUppercaseFlag bool, passwordType int) string {
+	var chars string = "abcdefghijklmnopqrstuvwxyz"
+
+	if includeNumbersFlag || passwordType == int(Random) {
+		chars += "0123456789"
+	}
+	if includeSymbolsFlag || passwordType == int(Random) {
+		chars += "!@#$%^&*()_+{}[]:;<>,.?/~`"
+	}
+	if includeUppercaseFlag || passwordType == int(Random) {
+		chars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	}
+
+	if passwordType == int(Pin) {
+		chars = "0123456789"
+	}
+
+	password := make([]byte, passwordLength)
+	for i := 0; i < passwordLength; i++ {
+		randomIndex, _ := rand.Int(rand.Reader, big.NewInt(int64(len(chars))))
+		password[i] = chars[randomIndex.Int64()]
+	}
+
+	return string(password)
 }
