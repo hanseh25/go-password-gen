a57debb209ecf3b2a7d8030fb6dc1d7ddb8ce262
hans torres <hans.torres@98labs.com>
2024-04-15 20:34:36 +0800

    add func 5

--- main.go
+++ main.go
@@ -27,7 +27,7 @@ func generate(passwordLength int, includeNumbersFlag bool, includeSymbolsFlag bo
 		chars += "!@#$%^&*()_+{}[]:;<>,.?/~`"
 	}
 	if includeUppercaseFlag || passwordType == int(Random) {
-		chars += "ABCDEFGHIJKLMNOPQRSTUVXYZ"
+		chars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 	}
 
 	if passwordType == int(Pin) {
